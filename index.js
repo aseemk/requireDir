8cc6f6f77b903fae07d1a5a5f6966be5506e51ea
Aseem Kishore <aseem.kishore@gmail.com>
2012-06-29 22:53:35 -0400

    Implementation: support returning duplicates!

--- index.js
+++ index.js
@@ -41,7 +41,9 @@ module.exports = function requireDir(dir, opts) {
     // we're recursing and can be ignored if we're not. if a basename has no
     // directory, then we'll follow Node's own require() algorithm of going
     // through and trying the require.extension keys in order. in the process,
-    // we create and return a map from basename to require()'d contents!
+    // we create and return a map from basename to require()'d contents! and
+    // if duplicates are asked for, we'll never short-circuit; we'll just add
+    // to the map using the full filename as a key also.
     var map = {};
 
     for (var base in filesForBase) {
@@ -63,15 +65,20 @@ module.exports = function requireDir(dir, opts) {
             if (FS.statSync(path).isDirectory()) {
                 if (opts.recurse) {
                     map[base] = requireDir(path, opts);
+
+                    // if duplicates are wanted, key off the full name too:
+                    if (opts.duplicates) {
+                        map[file] = map[base];
+                    }
                 }
             } else {
                 filesMinusDirs[file] = path;
             }
         }
 
-        // if we're recursing and already encountered a directory for this
-        // basename, we're done for this basename:
-        if (map[base]) {
+        // if we're recursing and we already encountered a directory for this
+        // basename, we're done for this base if we're ignoring duplicates:
+        if (map[base] && !opts.duplicates) {
             continue;
         }
 
@@ -87,8 +94,19 @@ module.exports = function requireDir(dir, opts) {
             var path = filesMinusDirs[file];
 
             if (path) {
-                map[base] = require(path);
-                break;
+                // if duplicates are wanted, key off the full name always, and
+                // also the base if it hasn't been taken yet (since this ext
+                // has higher priority than any that follow it). if duplicates
+                // aren't wanted, we're done with this basename.
+                if (opts.duplicates) {
+                    map[file] = require(path);
+                    if (!map[base]) {
+                        map[base] = map[file];
+                    }
+                } else {
+                    map[base] = require(path);
+                    break;
+                }
             }
         }
     }
