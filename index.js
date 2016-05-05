2dd7dd19410b231ebbf4823345451976378810e1
Murillo Freitas <mmbfreitas@hotmail.com>
2016-05-05 20:16:08 -0300

    Update index.js

--- index.js
+++ index.js
@@ -74,7 +74,7 @@ module.exports = function requireDir(dir, opts) {
                     }
 
                     map[base] = requireDir(path, opts);
-
+                    map[base].is_directory = true;
                     // if duplicates are wanted, key off the full name too:
                     if (opts.duplicates) {
                         map[file] = map[base];
